ed944812272e9781fea05040fa03ace53c16ea14
RRe36 <15215556+rre36@users.noreply.github.com>
2018-11-08 18:23:01 +0100

    Update 2018-11-08/01/01.ino

--- 2018-11-08/01/01.ino
+++ 2018-11-08/01/01.ino
@@ -1,9 +1,11 @@
 float runtime;
 float randomNumber;
 float randomLED;
-float scoreLedOn;
-float scoreLedButton;
-float scoreLedResult;
+int i=0;
+unsigned long scoreLedOn;
+unsigned long scoreLedButton;
+unsigned long scoreLedResult;
+unsigned long scoreFinal;
 
 int LedRed = 8;
 int LedGreen = 9;
@@ -15,7 +17,9 @@ bool stateGreen;
 bool stateLedRed;
 bool stateLedGreen;
 
-int scoreTime() {
+bool gateBool;
+
+unsigned long scoreTime() {
   return (millis());
 }
 
@@ -30,16 +34,16 @@ randomSeed(analogRead(0));
 }
 
 void loop() {
-
+ while(i<1){
   if (digitalRead(buttonRed) == LOW) {
     stateRed = 1;
-    } else {
+    } else if (digitalRead(buttonRed) == HIGH) {
     stateRed = 0;  
     }
 
   if (digitalRead(buttonGreen) == LOW) {
     stateGreen = 1;
-    } else {
+    } else if (digitalRead(buttonGreen) == HIGH) {
     stateGreen = 0;  
     }
     
@@ -60,22 +64,75 @@ if (ledOn < 1) {
   }
   while (digitalRead(buttonRed) == LOW) {
     stateLedRed = 0;
-
-    scoreLedButton = scoreTime();
+ //Serial.println("WHILE BUTTON RED");  
     }
+
+  if (digitalRead(buttonRed) == LOW && stateLedRed==1){
+      scoreLedButton = scoreTime();
+//Serial.println("IF BUTTON RED");
+      /*
+      gateBool=0;
+      } else {scoreLedButton=scoreLedButton;
+      */
+      }
 
    while (digitalRead(buttonGreen) == LOW) {
     stateLedGreen = 0;
+    //Serial.println("WHILE BUTTON GREEN");
+    }
 
+  if (digitalRead(buttonGreen) == LOW && stateLedGreen==1){
+      scoreLedButton = scoreTime();
+//Serial.println("IF BUTTON GREEN");
+      /*
+      gateBool=0;
+      } else {scoreLedButton=scoreLedButton;
+      */
+      }
+
+    /*
+    if (stateLedGreen=1) {
     scoreLedButton = scoreTime();
     }
+    */
+
+
+scoreLedResult = scoreLedButton-scoreLedOn; 
+Serial.print(scoreLedButton);
+Serial.print(" ");
+Serial.println(scoreLedOn);
+
 /*
-scoreLedResult = scoreLedButton-scoreLedOn;
-Serial.println(scoreLedResult); 
+Serial.print(digitalRead(buttonGreen));
+Serial.print(digitalRead(buttonRed));
+Serial.print(stateLedGreen);
+Serial.println(stateLedRed);
 */
 
-Serial.println(stateRed);
+
+//if ((digitalRead(buttonGreen) == LOW && stateLedGreen==1) || (digitalRead(buttonRed) == LOW && stateLedRed==1)) {
+  scoreFinal=scoreLedResult;
+Serial.println(scoreLedResult);
+delay(2000);
+   //}
+   
+  //Serial.println(digitalRead(buttonGreen));
   
+
+/*
+  if (gateBool=0) {
+    Serial.println(scoreFinal);
+    delay(10);
+   gateBool=1;
+    }
+*/
+    
+  
+  
+ 
+
 digitalWrite(LedRed, stateLedRed);
 digitalWrite(LedGreen, stateLedGreen);
+}
+i++;
 }
