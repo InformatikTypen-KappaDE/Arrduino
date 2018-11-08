20ce4f788d879f194faf02d70c98fc80b28a78b1
RRe36 <15215556+rre36@users.noreply.github.com>
2018-11-08 14:54:26 +0100

    Update 2018-11-08/01/01.ino

--- 2018-11-08/01/01.ino
+++ 2018-11-08/01/01.ino
@@ -1,9 +1,16 @@
 float runtime;
+int LedRed = 8;
+int LedGreen = 9;
+int buttonRed = 11;
+int buttonGreen = 12;
 
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+pinMode (LedRed, OUTPUT);
+pinMode (LedGreen, OUTPUT);
+pinMode (buttonRed, INPUT_PULLUP);
+pinMode (buttonGreen, INPUT_PULLUP);
 }
 
 void loop() {
