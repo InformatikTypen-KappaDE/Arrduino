3077de5699cd41e81b6070659352eaef5ced3fa5
RRe36 <15215556+rre36@users.noreply.github.com>
2018-11-08 15:30:49 +0100

    Update 2018-11-08/01/01.ino

--- 2018-11-08/01/01.ino
+++ 2018-11-08/01/01.ino
@@ -1,4 +1,10 @@
 float runtime;
+float randomNumber;
+float randomLED;
+float scoreLedOn;
+float scoreLedButton;
+float scoreLedResult;
+
 int LedRed = 8;
 int LedGreen = 9;
 int buttonRed = 11;
@@ -6,6 +12,12 @@ int buttonGreen = 12;
 
 bool stateRed;
 bool stateGreen;
+bool stateLedRed;
+bool stateLedGreen;
+
+int scoreTime() {
+  return (millis());
+}
 
 void setup() {
   Serial.begin(9600);
@@ -13,9 +25,39 @@ pinMode (LedRed, OUTPUT);
 pinMode (LedGreen, OUTPUT);
 pinMode (buttonRed, INPUT_PULLUP);
 pinMode (buttonGreen, INPUT_PULLUP);
+
+randomSeed(analogRead(0));
 }
 
 void loop() {
-  stateRed;
+  stateRed = digitalRead(buttonRed);
+  stateGreen = digitalRead(buttonGreen);
+  randomNumber = random(5);
+  randomLED = random(400);
+  float ledOn = stateLedRed+stateLedGreen;
+if (ledOn < 1) {
+  delay(randomNumber*1000);
+  scoreLedOn = scoreTime();
+  if (randomLED > 200) {
+    stateLedRed=1;
+    }
+    else {
+    stateLedGreen=1;
+    }
+    
+  }
+  while (digitalRead(buttonRed) == LOW) {
+    stateLedRed = 0;
 
+    scoreLedButton = scoreTime();
+    }
+
+   while (digitalRead(buttonGreen) == LOW) {
+    stateLedGreen = 0;
+
+    scoreLedButton = scoreTime();
+    }
+  
+digitalWrite(LedRed, stateLedRed);
+digitalWrite(LedGreen, stateLedGreen);
 }
