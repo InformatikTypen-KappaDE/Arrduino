ae716c31b838ef0a2dbe0e6f5bc126a6276e4efa
Benedikt9 <b.staudacher@gmx.net>
2019-01-17 18:02:55 +0100

    cfghj

--- GDI/Hackathon_5_Harz4Empf_nger/Hackathon_5_Harz4Empf_nger.ino
+++ GDI/Hackathon_5_Harz4Empf_nger/Hackathon_5_Harz4Empf_nger.ino
@@ -1,19 +1,42 @@
 #include <SoftwareSerial.h>
-
+String signalsent
 SoftwareSerial inputSerial(10, 11);
-
+bool valid = 1;
 int pinLed1R;
 int pinLed2Y;
 int pinLed3G;
 int pinLed4R;
 int pinLed5Y;
-
+bool b2;
+bool b3;
+bool b4;
+bool b5;
+bool b6;
 void setup() {
   Serial.begin(9600);
   inputSerial.begin(9600);
 }
 
 void loop() {
-  Serial.println(inputSerial.read);
+  Serial.println(inputSerial.readString());
+  signalsent = inputSerial.readString();
+  if (signalsent[0] = '^' && signalsent[1] = 'W' && signalsent[10] = '$' &&
+                      signalsent[11] = '\n') {
+    Sendbyte = inputSerial.readString().substring(2, 9);
+    for (int i = 0; i < 8; i++) {
+      if (valid && (Sendbyte[i] = 0 || Sendbyte[i] = 1)) {
+Sendbyte[5] = b6;
+  Sendbyte[6] = b5;
+  Sendbyte[7] = b4;
+  Sendbyte[8] = b3;
+  Sendbyte[9] = b2;
+      } else if (i = 0) {
+        valid = 1;
+      } else {
+        valid = 0;
+      }
+    }
+  }
 
+}
 }
--- GDI/Hackathon_5_Sender/Hackathon_5_Sender.ino
+++ GDI/Hackathon_5_Sender/Hackathon_5_Sender.ino
@@ -1,21 +1,32 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial outputSerial(10, 11);
-int pinB2;
-int pinB3;
-int pinB4;
-int pinB5;
-int pinB6;
+int pinB2 = 2;
+int pinB3 = 3;
+int pinB4 = 4;
+int pinB5 = 5;
+int pinB6 = 6;
 
-bool b2;
-bool b3;
-bool b4;
-bool b5;
-bool b6;
+bool b2;    //Fernsehen
+bool b3;    //Putzen
+bool b4;    //Hausverlassen
+bool b5;    //Scharfes Hinsehen Gelbes und Rotes Licht
+bool b6;    //Licht im Treppenhaus
+
+bool prevB2;
+bool prevB3;
+bool prevB4;
+bool prevB5;
+bool prevB6;
 
 void setup() {
   Serial.begin(9600);
   outputSerial.begin(9600);
+  pinMode(pinB2, INPUT);
+  pinMode(pinB3, INPUT);
+  pinMode(pinB4, INPUT);
+  pinMode(pinB5, INPUT);
+  pinMode(pinB6, INPUT);
 }
 
 char sendByte[12];
@@ -27,18 +38,51 @@ void loop() {
   b5 = digitalRead(pinB5);
   b6 = digitalRead(pinB6);
 
-  sendByte[0] = '^';
-  sendByte[1] = 'W';
-  sendByte[2] = '0';
-  sendByte[3] = '0';
-  sendByte[4] = '0';
-  sendByte[5] = b6;
-  sendByte[6] = b5;
-  sendByte[7] = b4;
-  sendByte[8] = b3;
-  sendByte[9] = b2;
-  sendByte[10] = '$';
-  sendByte[11] = '\n';
-  
-  outputSerial.write(sendByte, 11);
+  if ((b2 != prevB2) || (b3 != prevB3) || (b4 != prevB4) || (b5 != prevB5) || (b6 != prevB6)) {
+    sendByte[0] = '^';
+    sendByte[1] = 'W';
+    sendByte[2] = '0';
+    sendByte[3] = '0';
+    sendByte[4] = '0';
+    if (b6 == HIGH) {
+      sendByte[5] = '1';
+    } else {
+      sendByte[5] = '0';
+    }
+    if (b5 == HIGH) {
+      sendByte[6] = '1';
+    } else {
+      sendByte[6] = '0';
+    }
+    if (b4 == HIGH) {
+      sendByte[7] = '1';
+    } else {
+      sendByte[7] = '0';
+    }
+    if (b3 == HIGH) {
+      sendByte[8] = '1';
+    } else {
+      sendByte[8] = '0';
+    }
+    if (b2 == HIGH) {
+      sendByte[9] = '1';
+    } else {
+      sendByte[9] = '0';
+    }
+    sendByte[10] = '$';
+    sendByte[11] = '\n';
+    outputSerial.write(sendByte, 11);
+    /*
+    Serial.print(sendByte);
+    Serial.println("");
+    */
+  }
+  prevB2 = b2;
+  prevB3 = b3;
+  prevB4 = b4;
+  prevB5 = b5;
+  prevB6 = b6;
+
+
+  delay(100);
 }
